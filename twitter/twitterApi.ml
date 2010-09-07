5f107b33b9832bf254c44f779dbb2ea5e24fb79e
yoshihiro503 <y.imai-at-ocaml.jp>
2010-09-07 23:13:16 +0900

    twitter dir

--- twitter/twitterApi.ml
+++ twitter/twitterApi.ml
@@ -1,11 +1,9 @@
-let ocamltter_consumer_key = "vS0nKAS6ieWL76zZaQgF4A"
-let ocamltter_consumer_secret = "XHa1ZiPcNRsYKw4mdIv8wHUiNulpBFxKT1ntXXuJgo"
-
 open Util
 open Util.Date
 open Http
 open Json
 open Oauth
+open OauthForTwitter
 
 exception TwErr of string
 
@@ -17,34 +15,34 @@ type tweet = {
     text: string;
   }
 
-let parse_date01 st = (* Wed Apr 21 02:29:17 +0000 2010 *)
-  let mon = pmonth @@ String.sub st 4 3 in
-  let day = int_of_string @@ String.sub st 8 2 in
-  let h = int_of_string @@ String.sub st 11 2 in
-  let m = int_of_string @@ String.sub st 14 2 in
-  let s = int_of_string @@ String.sub st 17 2 in
-  let year = int_of_string @@ String.sub st 26 4 in
-  Date.make_from_gmt year mon day h m s
-
-let parse_date02 st = (* Sat, 17 Apr 2010 08:23:55 +0000 *)
-  let mon = pmonth @@ String.sub st 8 3 in
-  let day = int_of_string @@ String.sub st 5 2 in
-  let h = int_of_string @@ String.sub st 17 2 in
-  let m = int_of_string @@ String.sub st 20 2 in
-  let s = int_of_string @@ String.sub st 23 2 in
-  let year = int_of_string @@ String.sub st 12 4 in
-  Date.make_from_gmt year mon day h m s
-
-let parse_date st =
-  try parse_date01 st with
-  | _ -> parse_date02 st
-
 let show_tweet t =
   let mo,d,h,m = Date.mon t.date, Date.day t.date, hour t.date, min t.date in
   !%" [%02d/%02d %02d:%02d] %s: %s %LdL" mo d h m t.sname t.text t.id
 
 let tw_compare t1 t2 = compare t1.date t2.date
     
+let parse_date st =
+  let parse_date01 st = (* Wed Apr 21 02:29:17 +0000 2010 *)
+    let mon = pmonth @@ String.sub st 4 3 in
+    let day = int_of_string @@ String.sub st 8 2 in
+    let h = int_of_string @@ String.sub st 11 2 in
+    let m = int_of_string @@ String.sub st 14 2 in
+    let s = int_of_string @@ String.sub st 17 2 in
+    let year = int_of_string @@ String.sub st 26 4 in
+    Date.make_from_gmt year mon day h m s
+  in
+  let parse_date02 st = (* Sat, 17 Apr 2010 08:23:55 +0000 *)
+    let mon = pmonth @@ String.sub st 8 3 in
+    let day = int_of_string @@ String.sub st 5 2 in
+    let h = int_of_string @@ String.sub st 17 2 in
+    let m = int_of_string @@ String.sub st 20 2 in
+    let s = int_of_string @@ String.sub st 23 2 in
+    let year = int_of_string @@ String.sub st 12 4 in
+    Date.make_from_gmt year mon day h m s
+  in
+  try parse_date01 st with
+  | _ -> parse_date02 st
+	  
 let json2status j =
     let date =
       Json.getf "created_at" j |> Json.as_string |> parse_date
@@ -76,16 +74,10 @@ let catch_twerr (f: 'a -> Json.t) (x : 'a) =
     | None -> j
   with
   | TwErr m as e -> raise e
-  | e -> failwith ("twitter error: "^Printexc.to_string e)
+  | e -> failwith ("unknown error: "^Printexc.to_string e)
 
 let twitter (tok,sec,verif) ?(host="api.twitter.com") meth cmd params =
-  let oauth = {
-    Oauth.consumer_key = ocamltter_consumer_key;
-    Oauth.consumer_secret = ocamltter_consumer_secret;
-    Oauth.access_token=tok;
-    Oauth.access_token_secret=sec;
-    Oauth.verif=verif
-  } in
+  let oauth = OauthForTwitter.oauth(tok,sec,verif) in
   let f () =
     Json.parse (Oauth.access oauth meth host cmd params)
   in
@@ -161,50 +153,5 @@ let search word =
 let rate_limit_status () =
   twitter_without_auth GET "/1/account/rate_limit_status.json" []
 
-let host = "api.twitter.com"
-
-
-let read_lines ch = slist "\n" id (read_all ch)
-let parse_http_params s = 
-  Str.split(Str.regexp"&") s |> 
-  List.map (fun s -> 
-    let l = Str.split (Str.regexp "=") s 
-    in try List.hd l, List.hd (List.tl l) with Failure _ -> raise (Failure ("can't parse"^s)))
-let read_params ch = parse_http_params (read_lines ch)
-let assoc key dic = try List.assoc key dic with Not_found -> raise (Failure (key ^ " not found"))
-
-let fetch_request_token () = 
-      Oauth.fetch_request_token 
-        ~host:host
-        ~path:"/oauth/request_token"
-        ~oauth_consumer_key:ocamltter_consumer_key
-        ~oauth_consumer_secret:ocamltter_consumer_secret
-        ()
-    (fun _ ch ->
-  let res = read_params ch in
-  let token, secret = assoc "oauth_token" res, assoc "oauth_token_secret" res in
-    "http://twitter.com/oauth/authorize?oauth_token="^token, token, secret)
-
-
-
-let fetch_access_token req_token req_secret verif = 
-  fetch_access_token 
-    ~http_method:GET
-    ~host:host
-    ~path:"/oauth/access_token"
-    ~oauth_consumer_key:ocamltter_consumer_key
-    ~oauth_consumer_secret:ocamltter_consumer_secret
-    ~oauth_token:req_token
-    ~oauth_token_secret:req_secret
-    ~verif:verif
-    ()
-    (fun _ ch -> 
-      let res = read_params ch in
-      let acc_token, acc_secret, user = 
-        assoc "oauth_token" res, 
-        assoc "oauth_token_secret" res,
-        assoc "screen_name" res
-      in user, acc_token, acc_secret
-    )
-
-
+let fetch_request_token = OauthForTwitter.fetch_request_token
+let fetch_access_token  = OauthForTwitter.fetch_access_token
