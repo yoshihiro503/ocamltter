084d5ece8e856e23ad3b91a7c594cfc7c40c055e
Jun Furuse <jun.furuse@gmail.com>
2014-10-21 19:02:52 +0000

    added uploads app

---
+++ flickr/uploads.ml
@@ -0,0 +1,16 @@
+open Spotlib.Spot
+open Flickr
+module Xml = Twitter.Xml
+
+let auth_file = "ocaml_flickr.auth"
+
+let o = get_oauth auth_file
+
+let rev_files = ref []
+
+let () = Arg.parse [] (fun x -> rev_files := x :: !rev_files) "uploads photoset photos.."
+
+let () = match List.rev !rev_files with
+  | photoset :: photos ->
+      Tools.uploads ~photoset photos o
+  | _ -> assert false
