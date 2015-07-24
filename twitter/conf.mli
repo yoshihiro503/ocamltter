5bca235265f8cfbcc03ece27e1a6199234c7294d
Jun Furuse <jun.furuse@gmail.com>
2015-07-24 16:29:06 +0000

    Annoying bug around package module type

--- twitter/conf.mli
+++ twitter/conf.mli
@@ -2,7 +2,7 @@
   
     This is equivalent with [Oauth_ex.Conf] but without [app]. *)
 
-val oauth_signature_method : Oauth.signature_method
+val oauth_signature_method : OCamltter_oauth.Oauth.signature_method
 val oauth_callback : string option option
 val host : string
 val request_path : string
